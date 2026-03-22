ea0cf0df784c6d4cbf07f129a83ac116775d1c71
Tashi 🏔️ <tashi@namche.ai>
2026-03-22 21:47:29 +0100

    fix: import jose jwtVerify (#35)
    * fix: restore gmail oidc ingress
    
    Co-authored-by: Jodok Batlogg <jodok@batlogg.com>
    
    * fix: import jose jwtVerify
    
    Co-authored-by: Jodok Batlogg <jodok@batlogg.com>
    
    ---------
    
    Co-authored-by: Jodok Batlogg <jodok@batlogg.com>

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import { existsSync, readFileSync } from 'node:fs';
 import { createHmac, timingSafeEqual } from 'node:crypto';
 
 import { serve } from '@hono/node-server';
+import { jwtVerify } from 'jose';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import yaml from 'js-yaml';
